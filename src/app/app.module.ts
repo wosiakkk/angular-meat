cbf2c1f10bdcbc939d91a6fb36fa3282585c0dca
Bruno Wosiak <brunofwosiak@gmail.com>
2020-01-02 20:28:02 -0300

    51

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import {ROUTES} from './app.routes'
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
+import { RestaurantService }  from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [RestaurantService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router'
+import { RestaurantService } from '../../restaurants/restaurants.service'
+import { MenuItem } from '../menu-item/menu-item.model';
+
+@Component({
+  selector: 'mt-menu',
+  templateUrl: './menu.component.html'
+})
+export class MenuComponent implements OnInit {
+
+  menu: Observable<MenuItem[]>
+
+  constructor(private restaurantService: RestaurantService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.menu = this.restaurantService
+      .menuOfRestaurant(this.route.parent.snapshot.params['id'])
+  }
+
+  addMenuItem(item: MenuItem){
+    console.log(item)
+  }
+
+}
